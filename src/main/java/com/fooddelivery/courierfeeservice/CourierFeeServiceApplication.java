87f81d15baf5a5bf67077eff0f81a3ca7da6a39c
AndreasR <andreas.rand@hotmail.com>
2024-03-31 14:26:59 +0300

    Changed main class naming

--- src/main/java/com/fooddelivery/courierfeeservice/CourierFeeServiceApplication.java
+++ src/main/java/com/fooddelivery/courierfeeservice/CourierFeeServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CourierfeeserviceApplication {
+public class CourierFeeServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CourierfeeserviceApplication.class, args);
+		SpringApplication.run(CourierFeeServiceApplication.class, args);
 	}
 
 }
